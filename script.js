a632802439ec369dcacddd4a1103c2b62f5588e1
chintoooo <jeevansatishbonthu123@gmail.com>
2024-10-18 18:10:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 //your JS code here. If required.
+const fetchPost = fetch("https://jsonplaceholder.typicode.com/posts/1").then(
+  (res) => res.json()
+); // 2
+
+const fetchUser = fetch("https://jsonplaceholder.typicode.com/users/1").then(
+  (res) => res.json()
+); // 3
+
+const fetchComment = fetch(
+  "https://jsonplaceholder.typicode.com/comments/1"
+).then((res) => res.json()); // 4
+
+Promise.all([fetchPost, fetchUser, fetchComment])
+  .then(([post, user, comment]) => {
+    console.log("🚀 ~ .then ~ comment:", comment);
+    console.log("🚀 ~ .then ~ user:", user);
+    console.log("🚀 ~ .then ~ post:", post);
+  })
+  .catch((error) => {
+    console.log("🚀 ~ error:", error);
+  });
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
